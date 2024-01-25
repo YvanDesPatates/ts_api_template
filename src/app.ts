2b01fb90700d2ce37439380ef9587fc1872f66bf
ROUX YVAN <yvan.roux@inrae.fr>
2024-01-25 15:42:28 +0100

    .env file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.PORT
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res?.send('Hello World!')
 })
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`)
+  console.log(`Express is listening at http://localhost:${port}`)
 })
