b0c6f21e0859f7a71278244f8041bc6ff2b5a6d8
ROUX YVAN <yvan.roux@inrae.fr>
2024-01-25 15:18:52 +0100

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:jest/recommended"
+        "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
         {
