db94b50207a156675267be488acbe24a64195c48
Yvan Roux <yvis99@gmail.com>
2024-02-01 22:28:23 +0100

    ts config strict : no implicit any

--- src/displayableErrors/asyncWrapperErrorCatchingMiddleware.ts
+++ src/displayableErrors/asyncWrapperErrorCatchingMiddleware.ts
@@ -3,5 +3,5 @@ import {NextFunction, Request, Response} from "express";
 /**
  * Used to wrap ASYNC controller functions in route files. This allows to simply throw errors even in async functions and ensure that errors will be pass to the ErrorHandler middleware using next() method.
  */
-export const asyncWrapper = (AsyncFunction: (req: Request, res: Response, next: NextFunction) => Promise<void>, controller) =>
+export const asyncWrapper = (AsyncFunction: (req: Request, res: Response, next: NextFunction) => Promise<void>, controller: any) =>
     (req: Request, res: Response, next: NextFunction) => AsyncFunction.bind(controller)(req, res, next).catch(next);
