416b69908e62fac6c27c4f3de951c841c25b1afa
Yvan Roux <yvis99@gmail.com>
2024-02-01 17:59:27 +0100

    mail for email

--- src/account/Account.ts
+++ src/account/Account.ts
@@ -1,16 +1,16 @@
 export class Account {
-    private mail: string;
+    private email: string;
     private name: string;
     private pwd?: string;
 
 
-    public constructor(uniqueMail: string, name: string, pwd?: string) {
-        this.mail = uniqueMail;
+    public constructor(uniqueEmail: string, name: string, pwd?: string) {
+        this.email = uniqueEmail;
         this.name = name;
         this.pwd = pwd;
     }
 
     public getCopyWithoutPwd(): Account{
-        return new Account(this.mail, this.name);
+        return new Account(this.email, this.name);
     }
 }
