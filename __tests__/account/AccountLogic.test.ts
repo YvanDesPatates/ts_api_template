14a8c9d54c30d9cdb1e107e6406db450a2e584ca
ROUX YVAN <yvan.roux@inrae.fr>
2024-02-29 11:42:06 +0100

    premier test utile

--- __tests__/account/AccountLogic.test.ts
+++ __tests__/account/AccountLogic.test.ts
@@ -1,5 +1,12 @@
+import {AccountLogic} from "../../src/account/AccountLogic";
+import {DisplayableJsonError} from "../../src/displayableErrors/DisplayableJsonError";
+
 describe("AccountLogic.ts tests", () => {
-    test("Math test", () => {
+    test("create account with existing email is impossible", () => {
+        const email = "email";
+        new AccountLogic(email, "name", 5, "pwd").create();
+        expect(() => new AccountLogic(email, "name", 5, "pwd").create())
+            .toThrow(DisplayableJsonError);
         expect(2 + 2).toBe(4);
     });
 });
