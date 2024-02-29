9fd53b008a5495ea482e99c02e7591ecf47c07b4
ROUX YVAN <yvan.roux@inrae.fr>
2024-02-29 17:55:42 +0100

    tests unitaires avec Mock du DAO

--- __tests__/account/AccountLogic.test.ts
+++ __tests__/account/AccountLogic.test.ts
@@ -1,12 +1,51 @@
 import {AccountLogic} from "../../src/account/AccountLogic";
 import {DisplayableJsonError} from "../../src/displayableErrors/DisplayableJsonError";
+import {AccountJsonDAO} from "../../src/account/accountJsonDAO";
+import {MissingAttributeError} from "../../src/displayableErrors/MissingAttributeError";
 
 describe("AccountLogic.ts tests", () => {
-    test("create account with existing email is impossible", () => {
-        const email = "email";
-        new AccountLogic(email, "name", 5, "pwd").create();
-        expect(() => new AccountLogic(email, "name", 5, "pwd").create())
+    beforeAll(() => {
+        //avoid saving test data in the database
+        mockDaocreate()
+    });
+
+    test("create account with unused email", async () => {
+        const idExistsMock = mockDaoIdExists(false);
+        const accountToCreate = new AccountLogic("emaileeeee", "name", 5, "pwd");
+        const createdAccount = accountToCreate.create();
+        expect(createdAccount).toBeInstanceOf(AccountLogic);
+
+        idExistsMock.mockReset();
+    })
+
+    test("create account without pwd is impossible", async () => {
+        expect(() => new AccountLogic("email", "name", 5).create())
+            .toThrow(MissingAttributeError);
+    });
+
+    test("create account with existing email is impossible", async () => {
+        const idExistsMock = mockDaoIdExists(true);
+        expect(() => new AccountLogic("email", "name", 5, "pwd").create())
             .toThrow(DisplayableJsonError);
-        expect(2 + 2).toBe(4);
+
+        idExistsMock.mockReset();
     });
 });
+
+//#region mocks methods
+function mockDaoIdExists(returnValue: boolean) {
+    return jest
+        .spyOn(AccountJsonDAO.prototype, 'idExists')
+        .mockImplementation((id) => {
+            return returnValue;
+        });
+}
+
+function mockDaocreate() {
+    return jest
+        .spyOn(AccountJsonDAO.prototype, 'create')
+        .mockImplementation((account) => {
+            return account;
+        });
+}
+//#endregion
