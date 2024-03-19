b19f1a0c522c6f3c8ce05688011456824d00e0a5
Yvan Roux <yvis99@gmail.com>
2024-03-19 17:13:30 +0100

    update AccountController.ts to match changes

--- src/account/AccountController.ts
+++ src/account/AccountController.ts
@@ -4,38 +4,38 @@ import {AccountLogic} from "./AccountLogic";
 export class AccountController {
 
     public async getAllAccounts(req: Request, res: Response, next: NextFunction): Promise<void> {
-        const accounts: AccountLogic[] = AccountLogic.getAll();
+        const accounts: AccountLogic[] = await AccountLogic.getAll();
         res.status(200).json(accounts);
     }
 
     public async getAccountById(req: Request, res: Response, next: NextFunction): Promise<void> {
         const email = req.params.email;
-        const account: AccountLogic = AccountLogic.getAccount(email);
+        const account: AccountLogic = await AccountLogic.getAccount(email);
         res.status(200).json(account);
     }
 
     public async createAccount(req: Request, res: Response, next: NextFunction): Promise<void> {
         const accountToCreate: AccountLogic = new AccountLogic(req.body.email, req.body.name, req.body.amount, req.body.pwd);
-        res.status(201).json(accountToCreate.create());
+        res.status(201).json(await accountToCreate.create());
     }
 
     public async updateAccount(req: Request, res: Response, next: NextFunction): Promise<void> {
         const actualEmail:string = req.params.email;
-        const accountToUpdate: AccountLogic = AccountLogic.getAccount(actualEmail);
+        const accountToUpdate: AccountLogic = await AccountLogic.getAccount(actualEmail);
         const updatedAccount: AccountLogic = new AccountLogic(
             req.body.email ?? accountToUpdate.email,
             req.body.name ?? accountToUpdate.name,
             req.body.amount ?? accountToUpdate.amount,
             req.body.pwd ?? accountToUpdate.pwd
         );
-        updatedAccount.update(actualEmail);
+        await updatedAccount.update(actualEmail);
         res.status(200).json(updatedAccount);
     }
 
     public async deleteAccount(req: Request, res: Response, next: NextFunction): Promise<void> {
         const email = req.params.email;
         const account: AccountLogic = new AccountLogic(email, email, 5);
-        account.delete();
+        await account.delete();
         res.status(200).json(account);
     }
 
