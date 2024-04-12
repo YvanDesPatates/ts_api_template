5ff1a1963bf47b79227e817de7d623222d73500a
Yvan Roux <yvis99@gmail.com>
2024-04-12 21:57:30 +0200

    tests mock AccountMongoDAO, not only AccountJsonDAO

--- __tests__/account/AccountLogic.test.ts
+++ __tests__/account/AccountLogic.test.ts
@@ -2,27 +2,31 @@ import {AccountLogic} from "../../src/account/AccountLogic";
 import {DisplayableJsonError} from "../../src/displayableErrors/DisplayableJsonError";
 import {AccountJsonDAO} from "../../src/account/DAL/accountJsonDAO";
 import {MissingAttributeError} from "../../src/displayableErrors/MissingAttributeError";
+import {AccountMongoDAO} from "../../src/account/DAL/AccountMongoDAO";
 
 const mockFunctions: Array<jest.SpyInstance> = [];
 
 describe("AccountLogic.ts tests", () => {
     beforeAll(() => {
         //avoid saving test data in the database
-        mockFunctions.push(mockDaocreate());
-        mockFunctions.push(mockDaoDelete());
+        mockFunctions.push(mockDaoJsoncreate());
+        mockFunctions.push(mockDaoJsonDelete());
+        mockFunctions.push(mockDaoMongocreate());
+        mockFunctions.push(mockDaoMongoDelete())
     });
 
     afterAll(() => {
-       mockFunctions.forEach(mockFunction => mockFunction.mockReset());
+        mockFunctions.forEach(mockFunction => mockFunction.mockReset());
     });
 
     test("create account with unused email", async () => {
-        const idExistsMock = mockDaoIdExists(async () => false);
+        const idExistsMock = mockDAOsIdExists(async () => false);
+
         const accountToCreate = new AccountLogic("emaileeeee", "name", 5, "pwd");
         const createdAccount = await accountToCreate.create();
         expect(createdAccount).toBeInstanceOf(AccountLogic);
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("create account without pwd is impossible", async () => {
@@ -38,79 +42,97 @@ describe("AccountLogic.ts tests", () => {
     });
 
     test("create account with existing email is impossible", async () => {
-        const idExistsMock = mockDaoIdExists();
+        const idExistsMock = mockDAOsIdExists();
         await expect(async () => await new AccountLogic("email", "name", 5, "pwd").create())
             .rejects
             .toThrow(DisplayableJsonError);
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("update account", async () => {
-        const idExistsMock = mockDaoIdExists(async id => id === "email");
+        const idExistsMock = mockDAOsIdExists(async id => id === "email");
         const updatedAccount = await new AccountLogic("newEmail", "name", 5, "pwd").update("email");
         expect(updatedAccount).toBeInstanceOf(AccountLogic);
         expect(updatedAccount).toHaveProperty("email", "newEmail");
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("update account with existing email is impossible", async () => {
-        const idExistsMock = mockDaoIdExists();
+        const idExistsMock = mockDAOsIdExists();
         await expect(async () => await new AccountLogic("newEmail", "name", 5, "pwd").update("email"))
             .rejects
             .toThrow(DisplayableJsonError);
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("update account without pwd is impossible", async () => {
-        const idExistsMock = mockDaoIdExists(async id => id === "email");
+        const idExistsMock = mockDAOsIdExists(async id => id === "email");
         await expect(async () => await new AccountLogic("newEmail", "name", 5).update("email"))
             .rejects
             .toThrow(MissingAttributeError);
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("delete account", async () => {
-        const idExistsMock = mockDaoIdExists();
+        const idExistsMock = mockDAOsIdExists();
         new AccountLogic("newEmail", "name", 5, "pwd").delete();
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
     test("delete account with non existing email throw 404 error", async () => {
-        const idExistsMock = mockDaoIdExists(async () => false);
+        const idExistsMock = mockDAOsIdExists(async () => false);
         await expect(async () => await new AccountLogic("newEmail", "name", 5, "pwd").delete())
             .rejects
             .toThrow(DisplayableJsonError);
 
-        idExistsMock.mockReset();
+        idExistsMock.forEach(mock => mock.mockReset());
     });
 
 });
 
 //#region mocks methods
 /**
- * Mock the idExists method of AccountJsonDAO
+ * Mock the idExists method of AccountJsonDAO and AccountMongoDAO
  * @param mockFunction is the return value the mocked function will return, by default it returns true
  */
-function mockDaoIdExists(mockFunction: (id: string) => Promise<boolean> = async () => true) {
-    return jest
-        .spyOn(AccountJsonDAO.prototype, 'idExists')
-        .mockImplementation(mockFunction);
+function mockDAOsIdExists(mockFunction: (id: string) => Promise<boolean> = async () => true) {
+    return [
+        jest
+            .spyOn(AccountJsonDAO.prototype, 'idExists')
+            .mockImplementation(mockFunction),
+        jest
+            .spyOn(AccountMongoDAO.prototype, 'idExists')
+            .mockImplementation(mockFunction)
+    ]
 }
 
-function mockDaocreate() {
+function mockDaoJsoncreate() {
     return jest
         .spyOn(AccountJsonDAO.prototype, 'create')
         .mockImplementation(async (account) => account);
 }
 
-function mockDaoDelete(returnValue: boolean = true) {
+function mockDaoJsonDelete(returnValue: boolean = true) {
     return jest
         .spyOn(AccountJsonDAO.prototype, 'delete')
-        .mockImplementation(async () =>  returnValue);
+        .mockImplementation(async () => returnValue);
 }
+
+function mockDaoMongocreate() {
+    return jest
+        .spyOn(AccountMongoDAO.prototype, 'create')
+        .mockImplementation(async (account) => account);
+}
+
+function mockDaoMongoDelete(returnValue: boolean = true) {
+    return jest
+        .spyOn(AccountMongoDAO.prototype, 'delete')
+        .mockImplementation(async () => returnValue);
+}
+
 //#endregion
